e83d8f41e371158d687fc959b8f966a898b3078a
Daniel Barnes <daniel.barnes18697@gmail.com>
2022-03-12 21:00:15 +0000

    Create PETRESCUE-CREATE.sql

---
+++ 06. Databases and SQL for Data Science with Python/01. Getting Started with SQL/PETRESCUE-CREATE.sql
@@ -0,0 +1,24 @@
+-- Drop the PETRESCUE table in case it exists
+drop table PETRESCUE;
+-- Create the PETRESCUE table 
+create table PETRESCUE (
+	ID INTEGER NOT NULL,
+	ANIMAL VARCHAR(20),
+	QUANTITY INTEGER,
+	COST DECIMAL(6,2),
+	RESCUEDATE DATE,
+	PRIMARY KEY (ID)
+	);
+-- Insert sample data into PETRESCUE table
+insert into PETRESCUE values 
+	(1,'Cat',9,450.09,'2018-05-29'),
+	(2,'Dog',3,666.66,'2018-06-01'),
+	(3,'Dog',1,100.00,'2018-06-04'),
+	(4,'Parrot',2,50.00,'2018-06-04'),
+	(5,'Dog',1,75.75,'2018-06-10'),
+	(6,'Hamster',6,60.60,'2018-06-11'),
+	(7,'Cat',1,44.44,'2018-06-11'),
+	(8,'Goldfish',24,48.48,'2018-06-14'),
+	(9,'Dog',2,222.22,'2018-06-15')
+	
+;
